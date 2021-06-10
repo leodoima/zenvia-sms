e761101a10386b51db65f680be6e2b27b69776c3
Leonardo dos Santos <leodoima@hotmail.com>
2021-06-09 21:23:54 -0300

    Ajustando informaçoes e comentarios do index

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ Zenvia - API para mensagens
 */
 
 const totalvoice = require('totalvoice-node');
-const client = new totalvoice("84f1d2eb31f60dd2733d18c15ee151d8");
+const client = new totalvoice("token");
 
 var resposta_usuario = false;
 var multi_sms = false;
 var data_criacao = '';
 
-client.sms.enviar("49999954487", "Até logo, até mais ver, bon voyage, arrivederci.", resposta_usuario, multi_sms, data_criacao)
+client.sms.enviar("49999999", "Até logo, até mais ver, bon voyage, arrivederci.", resposta_usuario, multi_sms, data_criacao)
     .then(function (data) {
         console.log(data)
     })
